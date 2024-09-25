d78bc7e7d713c070cc65dd0f3a92751a0ebd1a7a
Terry52139 <108207362+Terry52139@users.noreply.github.com>
2024-09-25 14:16:04 +0530

    Add files via upload

---
+++ docs/docs/TB Display/TBscoreboard.js
@@ -0,0 +1,60 @@
+$("#inputButton1").click(function() {
+    // Prompt the user for input
+    const userInput = prompt("Please enter something:");
+    
+    // Display the input in the div
+    if (userInput !== null) {
+      // Display the input in the table row
+      $("#displayTable1").text(userInput);
+    }
+    $(this).blur();
+  });
+  
+$("#inputButton2").click(function() {
+    // Prompt the user for input
+    const userInput = prompt("Please enter something:");
+    
+    // Display the input in the div
+    if (userInput !== null) {
+      // Display the input in the table row
+      $("#displayTable2").text(userInput);
+    }
+    $(this).blur();
+  });
+  
+$("#inputButton3").click(function() {
+    // Prompt the user for input
+    const userInput = prompt("Please enter something:");
+    
+    // Display the input in the div
+    if (userInput !== null) {
+      // Display the input in the table row
+      $("#displayTable3").text(userInput);
+    }
+    $(this).blur();
+  });
+  
+$("#inputButton4").click(function() {
+    // Prompt the user for input
+    const userInput = prompt("Please enter something:");
+    
+    // Display the input in the div
+    if (userInput !== null) {
+      // Display the input in the table row
+      $("#displayTable4").text(userInput);
+    }
+    $(this).blur();
+  });
+  
+$("#inputButton5").click(function() {
+    // Prompt the user for input
+    const userInput = prompt("Please enter something:");
+    
+    // Display the input in the div
+    if (userInput !== null) {
+      // Display the input in the table row
+      $("#displayTable5").text(userInput);
+    }
+    $(this).blur();
+  });
+  
